5b2a5f8d2552b1a5b4a6ffb422ba606af8fbaf1e
Carlos Meza <carlos@digitalr00ts.com>
2018-09-28 00:13:53 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
 
 """The setup script."""
 
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
-with open(os.path.join(here, "requests", "__version__.py"), "r", "utf-8") as f:
-    exec(f.read(), about)
+with open(os.path.join(here, "atlas_ci", "__version__.py"), "r") as f:
+    exec(f.read(), about)  # pylint: disable=exec-used
 
-with open("README.md", "r", "utf-8") as f:
+with open("README.rst", "r") as f:
     readme = f.read()
-with open("HISTORY.md", "r", "utf-8") as f:
+with open("HISTORY.rst", "r") as f:
     history = f.read()
 
 requirements = []
